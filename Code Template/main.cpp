40762cad4d8e624b936488eef7c33c1775fb6faf
rahul1809 <rahul.mishra2003@gmail.com>
2021-06-13 23:22:42 +0530

    Added code template for C++

---
+++ Code Template/main.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//Useful Macros 
+#define MOD 1000000007
+#define f first 
+#define s second
+#define pb push_back
+#define mp make_pair
+
+#define REP(i,a,b,inc) (for int i = a, i<=b;i+=inc)
+
+//Shortening Code
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi; 
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+
+    cout<<"Hello world";
+    return 0;
+}
